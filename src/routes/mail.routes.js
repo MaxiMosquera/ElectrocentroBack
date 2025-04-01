86d1e98837d39cecc43ab3dc6a14c08b81cbf497
mmosquera <maximiliano.mosquera1@gmail.com>
2025-03-31 22:12:32 -0300

    cambios menores

--- src/routes/mail.routes.js
+++ src/routes/mail.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { resetPassword, confirmRegister, contactForm } from '../controllers/mail.controller.js';
+import { validateToken } from '../middlewares/logger.js';
 
 const router = express.Router();
 
 router.post('/reset-password', resetPassword);
 router.post('/confirm-register', confirmRegister);
-router.post('/contact', contactForm);
+router.post('/contact',validateToken, contactForm);
 
 export default router;
