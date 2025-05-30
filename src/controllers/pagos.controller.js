04c7d4bd82dd1a09c26a21525b30a65a4c7bb6ea
Apptrader <proyectoapptrader@gmail.com>
2025-05-30 14:02:27 -0300

    merge

--- src/controllers/pagos.controller.js
+++ src/controllers/pagos.controller.js
@@ -7,85 +7,85 @@ import OrderProduct from "../models/orderProducts.model.js";
 
 dotenv.config()
 
-export const createPreference = async (req, res) => {
-  try {
-    const { items, metadata } = req.body;
+  export const createPreference = async (req, res) => {
+    try {
+      const { items, metadata } = req.body;
 
-    if (!Array.isArray(items)) {
-      return res.status(400).json({ error: "La propiedad 'items' no es un arreglo" });
-    }
-
-   console.log(`${process.env.FRONTEND_URL}/compra-exitosa`)
-
-    // 1. Creamos la orden con estado "pending"
-    const newOrder = await Orden.create({
-      paymentId: null, // De momento no tenemos el ID de pago
-      user_id: metadata.customer.id,
-      finalPrice: parseFloat(metadata.total),
-      discountPercentage: 0,
-      status: "pending",
-    });
-
-    // 2. Creamos los productos de la orden (OrderProducts)
-    for (const item of metadata.items) {
-      let productType;
-      switch (item.category) {
-        case "motores":
-          productType = "motor";
-          break;
-        case "reductores":
-          productType = "reductor";
-          break;
-        case "convertidores":
-          productType = "convertidor";
-          break;
-        case "arranqueSuave":
-          productType = "arranqueSuave";
-          break;
-        default:
-          productType = item.category; // O maneja un fallback
+      if (!Array.isArray(items)) {
+        return res.status(400).json({ error: "La propiedad 'items' no es un arreglo" });
       }
 
-      await OrderProduct.create({
-        order_id: newOrder.id,
-        product_id: item.id,
-        product_type: productType,
-        quantity: item.quantity,
-        unitPrice: item.price,
-        fijacion_salida_id: item?.eje_obj?.id || null,
-        eje_salida_id: item?.eje_salida?.id || null,
-        tipo_entrada_id: item?.tipo_entrada_obj?.id || null,
+    console.log(`${process.env.FRONTEND_URL}/compra-exitosa`)
+
+      // 1. Creamos la orden con estado "pending"
+      const newOrder = await Orden.create({
+        paymentId: null, // De momento no tenemos el ID de pago
+        user_id: metadata.customer.id,
+        finalPrice: parseFloat(metadata.total),
+        discountPercentage: 0,
+        status: "pending",
       });
+
+      // 2. Creamos los productos de la orden (OrderProducts)
+      for (const item of metadata.items) {
+        let productType;
+        switch (item.category) {
+          case "motores":
+            productType = "motor";
+            break;
+          case "reductores":
+            productType = "reductor";
+            break;
+          case "convertidores":
+            productType = "convertidor";
+            break;
+          case "arranqueSuave":
+            productType = "arranqueSuave";
+            break;
+          default:
+            productType = item.category; // O maneja un fallback
+        }
+
+        await OrderProduct.create({
+          order_id: newOrder.id,
+          product_id: item.id,
+          product_type: productType,
+          quantity: item.quantity,
+          unitPrice: item.price,
+          fijacion_salida_id: item?.eje_obj?.id || null,
+          eje_salida_id: item?.eje_salida?.id || null,
+          tipo_entrada_id: item?.tipo_entrada_obj?.id || null,
+        });
+      }
+
+      // 3. Guardamos el orderId dentro de metadata para usarlo en el webhook
+      metadata.orderId = newOrder.id;
+
+      // 4. Creamos el objeto de preferencia de MP,
+      //    incluyendo la metadata actualizada con el orderId
+      const preferenceBody = {
+        items: items.map((item) => ({
+          title: item.name,
+          quantity: item.quantity,
+          unit_price: item.price || 1,
+          currency_id: "ARS",
+        })),
+        back_urls: {
+          success: `${process.env.FRONTEND_URL}/compra-exitosa`,
+          failure: `${process.env.FRONTEND_URL}/compra-fallida`,
+          pending: `${process.env.FRONTEND_URL}/pending`,
+        },
+        auto_return: "approved", 
+        metadata, // aquí ya va el "orderId" agregado
+      };
+
+      const preference = new Preference(mercadoClient);
+      const result = await preference.create({ body: preferenceBody });
+
+      // 5. Devolvemos la respuesta al frontend con la info de MercadoPago
+      return res.json({ id: result.id, init_point: result.init_point });
+    } catch (error) {
+      console.error("Error al crear la preferencia:", error);
+      return res.status(500).json({ error: "Error al crear la preferencia" });
     }
-
-    // 3. Guardamos el orderId dentro de metadata para usarlo en el webhook
-    metadata.orderId = newOrder.id;
-
-    // 4. Creamos el objeto de preferencia de MP,
-    //    incluyendo la metadata actualizada con el orderId
-    const preferenceBody = {
-      items: items.map((item) => ({
-        title: item.name,
-        quantity: item.quantity,
-        unit_price: item.price || 1,
-        currency_id: "ARS",
-      })),
-      back_urls: {
-        success: `${process.env.FRONTEND_URL}/compra-exitosa`,
-        failure: `${process.env.FRONTEND_URL}/compra-fallida`,
-        pending: `${process.env.FRONTEND_URL}/pending`,
-      },
-
-      metadata, // aquí ya va el "orderId" agregado
-    };
-
-    const preference = new Preference(mercadoClient);
-    const result = await preference.create({ body: preferenceBody });
-
-    // 5. Devolvemos la respuesta al frontend con la info de MercadoPago
-    return res.json({ id: result.id, init_point: result.init_point });
-  } catch (error) {
-    console.error("Error al crear la preferencia:", error);
-    return res.status(500).json({ error: "Error al crear la preferencia" });
-  }
-};
+  };
--- src/routes/mail.routes.js
+++ src/routes/mail.routes.js
@@ -2,10 +2,10 @@ import express from 'express';
 import { resetPassword, confirmRegister, contactForm } from '../controllers/mail.controller.js';
 import { validateToken } from '../middlewares/logger.js';
 
-const router = express.Router();
+const   router = express.Router();
 
 router.post('/reset-password', resetPassword);
 router.post('/confirm-register', confirmRegister);
-router.post('/contact',validateToken, contactForm);
+router.post('/contact', contactForm);
 
 export default router;
